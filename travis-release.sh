87ab184a32c96e0bce9c1d7e25f86344b682a623
Matt Sicker <boards@gmail.com>
2018-04-04 12:50:38 -0500

    Add missing step in release process

--- travis-release.sh
+++ travis-release.sh
@@ -6,6 +6,7 @@ enc_key=$1
 enc_iv=$2
 
 openssl aes-256-cbc -K ${enc_key} -iv ${enc_iv} -in .travis.tar.gz.enc -out .travis.tar.gz -d
+tar -xzf .travis.tar.gz
 eval "$(ssh-agent -s)"
 ssh-add travis/deploy_key
 git config user.name "Matt Sicker"
